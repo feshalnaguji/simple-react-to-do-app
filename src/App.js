5d1bb4a092dde140a541327984197fc38c43be5f
feshalnaguji <fesalnaguji@gmail.com>
2023-05-16 19:24:33 +0530

    simple futuristing to do app

--- src/App.js
+++ src/App.js
@@ -1,64 +1,47 @@
-import { useState } from "react";
-
-import Todos from "./components/Todos";
-import Button from "./UI/Button";
-import Input from "./UI/Input";
-import Form from "./UI/Form";
-
-import classes from "./App.module.css";
-
-const DUMMY_TODOS = [
-  { id: 1, task: "Create project", completed: false },
-  { id: 2, task: "Create 2nd project", completed: false },
-];
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
-  const [isEmpty, setIsEmpty] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
-    if (event.target[0].value.trim() === "") {
-      setIsEmpty(true);
-    } else {
-      setIsEmpty(false);
-      const newTodo = [
-        ...todos,
-        { id: Math.random(), task: event.target[0].value, status: "pending" },
-      ];
-      setTodos(newTodo);
-      event.target[0].value = "";
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== "") {
+      setTodos([...todos, inputValue]);
+      setInputValue("");
     }
   };
 
-  const checkBoxChangeHandler = (todoId) => {
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === todoId) {
-        return { ...todo, completed: !todo.completed };
-      }
-      return todo;
-    });
-
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
     setTodos(updatedTodos);
   };
 
-  const deleteTodoHandler = (todoId) => {
-    setTodos(todos.filter((todo) => todo.id !== todoId));
-  };
-
   return (
-    <div className={classes.app}>
-      <Form onClick={onSubmitHandler}>
-        <Input type="text" />
-        <Button>Add</Button>
-      </Form>
-      {isEmpty && <p>Input shouldn't be empty.</p>}
-      <Todos
-        onClick={deleteTodoHandler}
-        onCheck={checkBoxChangeHandler}
-        todo={todos}
-      />
+    <div className="todo-list-container">
+      <h1>Futuristic ToDo App</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Enter a todo"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <li key={index} className="todo-item">
+            <input type="checkbox" />
+            <label>{todo}</label>
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
